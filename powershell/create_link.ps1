5e928897867388b387e868fd1938a12e7f22b07e
kiwings <Ssmsk34j3@gmail.com>
2019-07-24 17:19:49 +0800

    Update create_link.ps1

--- powershell/create_link.ps1
+++ powershell/create_link.ps1
@@ -1,8 +1,8 @@
 $padding = '                                                                                                                                                                                                                                      '
 
-$file = Get-Content "D:\Users\sangfor\Desktop\testlnk\test.txt"
+$file = Get-Content "D:\Users\abc\Desktop\testlnk\test.txt"
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("D:\Users\sangfor\Desktop\testlnk\test.lnk")
+$Shortcut = $WshShell.CreateShortcut("D:\Users\abc\Desktop\testlnk\test.lnk")
 $Shortcut.TargetPath = "%SystemRoot%\system32\cmd.exe"
 $Shortcut.IconLocation = "%SystemRoot%\System32\Shell32.dll,21"
 $Shortcut.WindowStyle = 7
